a1d44b1f213512be53c596fd5228f8db5b46dded
Kalimaha <guido.barbaglia@gmail.com>
2019-08-11 13:31:47 +1000

    Initial setup

---
+++ src/main/java/au/com/mebank/transactions/analyser/Main.java
@@ -0,0 +1,11 @@
+package au.com.mebank.transactions.analyser;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hallo, world!");
+        for (String s: args) {
+            System.out.println(s);
+        }
+        System.out.println("Goodbye!");
+    }
+}
